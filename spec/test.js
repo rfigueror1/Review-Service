97ae0a5188b909c90d846ca2b6f1432cdb670be2
Adam Carter <carter.c.adam@gmail.com>
2018-07-19 16:36:18 -0700

    added testing suite

---
+++ spec/test.js
@@ -0,0 +1,28 @@
+const puppeteer = require('puppeteer');
+const pageURL = 'http://localhost:3002';
+
+let page;
+let browser;
+const width = 1280;
+const height = 720;
+
+beforeAll(async () => {
+  browser = await puppeteer.launch({
+    headless: false,
+    slowMo: 80,
+    args: [`--window-size=${width},${height}`]
+  });
+  page = await browser.newPage();
+  await page.setViewport({ width, height });
+});
+
+afterAll(() => {
+  browser.close();
+});
+
+describe('something to test', () => {
+
+  test('some kind of test', () => {
+
+  });
+})
