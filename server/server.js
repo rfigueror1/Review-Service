13b373af84873bf0841f5914eaa062adb3f32d98
Adam Carter <carter.c.adam@gmail.com>
2018-07-23 14:27:32 -0700

    server is retrieving data from data queries

--- server/server.js
+++ server/server.js
@@ -1,7 +1,7 @@
 
 const express = require('express');
 const path = require('path');
-const bodyParser = require('bodyParser');
+const bodyParser = require('body-parser');
 const db = require('../database/operations.js');
 
 const app = express();
@@ -15,7 +15,28 @@ app.use(express.static(path.join(__dirname, '/../client/dist')));
 
 app.get('/api/listing/:listingid/overview', (req, res) => {
   const listing_id = Number(req.params.listingid);
-  
+  console.log(listing_id);
+
+  db.getRatings(listing_id, function(err, results) {
+    if (err) {
+      console.log('err in server - overview: ', err)
+    }
+
+    console.log(results);
+  });
+});
+
+app.get('/api/listing/:listingid/reviews', (req, res) => {
+  const listing_id = Number(req.params.listingid);
+  console.log(listing_id);
+
+  db.getReviews(listing_id, function(err, results) {
+    if (err) {
+      console.log('err in server - reviews: ', err)
+    }
+
+    console.log(results);
+  });
 });
 
 app.listen(3002, console.log('Listening on port 3002'));
