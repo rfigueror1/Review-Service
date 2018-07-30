639f5bd4dec0b4cf08f4aac136885a0d8b1d266b
Adam Carter <carter.c.adam@gmail.com>
2018-07-29 19:51:06 -0700

    testing

--- spec/test.js
+++ spec/test.js
@@ -21,12 +21,18 @@ afterAll(() => {
   browser.close();
 });
 
-describe('something to test', () => {
-  // beforeEach(async () => {
-  //   await page.goto(pageURL, {waitUntil: 'networkidle2'});
-  // });
+describe('end to end tests', () => {
+  beforeEach(async () => {
+    await page.goto(pageURL, {waitUntil: 'networkidle2'});
+  });
 
-  test('some kind of test', () => {
+  test('pagination should have correct number of items', async () => {
+    const pagination = '.pagination-main-1ECuWn-';
+    const children = await page.$eval(pagination, e => e.textContent);
+    expect(children.length).toEqual(6);
+  });
+
+  test('clicking right page switch should change number of pages in pagination section', () => {
 
   });
 });
